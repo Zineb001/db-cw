af00367c4493aa25bb4cb4c842fde51caca3aff6
esksetareh <esksetareh@gmail.com>
2024-03-05 22:10:05 +0000

    fron-end movie search

---
+++ react-frontend/src/components/DiscoverGenres.jsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import styled from "styled-components";
+import { Link } from 'react-router-dom';
+
+function DiscoverGenres() {
+  return (
+    <div>
+      <h1>Discover Genres</h1>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/search-movie">Search Movie</Link></li>
+          <li><Link to="/future-releases">Future Releases</Link></li>
+          <li><Link to="/discover-genres">Discover Genres</Link></li>
+        </ul>
+      </nav>
+      {/* The rest of  this page's content */}
+    </div>
+  );
+}
+
+export default DiscoverGenres;
