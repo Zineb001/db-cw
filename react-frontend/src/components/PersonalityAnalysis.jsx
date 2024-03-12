99e0193746b825fb7701ed7d67c4974f8dfeb869
Zineb001 <zineb.flahy@gmail.com>
2024-03-12 19:37:56 +0000

    personality page

--- react-frontend/src/components/PersonalityAnalysis.jsx
+++ react-frontend/src/components/PersonalityAnalysis.jsx
@@ -4,14 +4,20 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 import NavigationBar from './NavigationBar';
 import { useState, useEffect} from 'react';
-import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { Select, MenuItem, FormControl, InputLabel, Box, Autocomplete, TextField } from '@mui/material';
+import { Bar } from 'react-chartjs-2';
+import {GenrePersonalitysHistogram} from './Charts'; 
 import './style.css';
 
 function PersonalityAnalysis() {
   const [ personalityMovies, setPersonalityMovies] = useState([]);
+  const [personalityGenres, setPersonalityGenres] = useState([]);
   const [searchMovies, setSearchMovies] = useState([]);
   const [selectedMovieId, setSelectedMovieId] = useState(personalityMovies[0]?.movie_id || '');
   const [isLoading, setIsLoading] = useState(true);
+
+  const hardcodedPersonalityTraits = ['Openness', 'Agreeableness', 'Emotional Stability', 'Conscientiousness', 'Extraversion'];
+  const [selectedTrait, setSelectedTrait] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,8 +28,13 @@ function PersonalityAnalysis() {
         setPersonalityMovies(personalityMoviesData);
 
         const searchMoviesResponse = await fetch('http://localhost:3001/api/searchMovies');
-        const searchMoviesData = await personalityMoviesResponse.json();
+        const searchMoviesData = await searchMoviesResponse.json();
         setSearchMovies(searchMoviesData);
+
+        const personalityGenreResponse = await fetch('http://localhost:3001/api/personalityGenres');
+        const personalityGenreData = await personalityGenreResponse.json();
+        const filteredPersonalityGenreData = personalityGenreData.filter(genre => genre.name !== "(no genres listed)");
+        setPersonalityGenres(filteredPersonalityGenreData);
   
   
       } catch (error) {
@@ -36,13 +47,18 @@ function PersonalityAnalysis() {
   }, []);
 
 
-  const handleChange = (event) => {
-    setSelectedMovieId(event.target.value);
+  const handleMovieChange = (event, newValue) => {
+    setSelectedMovieId(newValue?.movie_id || '');
   };
 
-  const selectedMovieData = personalityMovies.find(movie => movie.movie_id === selectedMovieId) || null;
+  const handleTraitChange = (event, newValue) => {
+    setSelectedTrait(newValue);
+  };
 
-  const chartData = selectedMovieData ? {
+
+  const selectedMovieData = personalityMovies.find(movie => movie.movie_id === selectedMovieId) || personalityMovies[0];
+
+  const movieChartData = selectedMovieData ? {
     labels: ['Openness', 'Agreeableness', 'Emotional Stability', 'Conscientiousness', 'Extraversion'],
     datasets: [
       {
@@ -61,6 +77,40 @@ function PersonalityAnalysis() {
     ],
   } : {};
 
+  const options = {
+    scales: {
+      x: {
+        ticks: {
+          color: 'white', 
+        },
+        title: {
+          display: true,
+          text: 'Personality Traits',
+          color: 'white',
+        },
+        grid: {
+          color: 'rgba(255, 255, 255, 0.1)', 
+        }
+      },
+      y: {
+        ticks: {
+          color: 'white', 
+        },
+        grid: {
+          color: 'rgba(255, 255, 255, 0.1)', 
+        }
+      }
+    },
+    plugins: {
+      legend: {
+        labels: {
+          color: 'white'
+        }
+      }
+    }
+  };
+  
+
   return (
     <ThemeProvider theme={theme}>
     <NavigationBar />
@@ -68,23 +118,149 @@ function PersonalityAnalysis() {
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        <div>
-          <FormControl fullWidth>
-            <InputLabel id="movie-select-label">Select a Movie</InputLabel>
-            <Select
-              labelId="movie-select-label"
-              id="movie-select"
-              value={selectedMovieId}
-              label="Select a Movie"
-              onChange={handleChange}
-            >
-              {personalityMovies.map((movie) => (
-                <MenuItem key={movie.movie_id} value={movie.movie_id}>{movie.movie_title}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          {selectedMovieData && <Bar data={chartData} />}
+      <>
+      <div sx={{paddingBottom: 200}}>
+        <div className="personality-container">
+          <div className="personality-menu-container">
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              '& a': {
+                color: 'white',
+                marginTop: 2,
+                textDecoration: 'none',
+                textAlign: 'center',
+                paddingLeft: 20,
+                paddingRight: 20,
+                font: "Verdana" 
+              }
+            }}
+          >
+            <Autocomplete
+              id="movie-autocomplete"
+              options={personalityMovies}
+              getOptionLabel={(option) => option.movie_title || ''}
+              value={personalityMovies.find(movie => movie.movie_id === selectedMovieId) || null}
+              onChange={handleMovieChange}
+              renderInput={(params) => (
+                <TextField {...params} label="Select a Movie" variant="outlined" fullWidth
+                  sx={{
+                  "& .MuiOutlinedInput-root": {
+                    bgcolor: '#E0E0E0', 
+                    "&.Mui-focused": {
+                      bgcolor: "#E0E0E0", 
+                      color: 'black',
+                    },
+                    "& input": {
+                      color: 'black', 
+                    }
+                  },
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    borderColor: '#333333',
+                  },
+                  "&:hover .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "#E0E0E0", 
+                  },
+                  "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                    borderColor: 'black', 
+                  },
+                }} 
+                />
+              )}
+              isOptionEqualToValue={(option, value) => option.movie_id === value.movie_id}
+              sx={{
+                width: 300, 
+                "& .MuiAutocomplete-inputRoot": {
+                  color: "#E0E0E0", 
+                },
+              }}
+            />
+            <a href="#" style={{ color: 'white' }}>Select a movie to display the correlation between the ratings and the users' personality traits</a>
+          </Box>
+          </div>
+          <div className="personality-histogram-container ">
+            {selectedMovieData && <Bar data={movieChartData} options={options} />}
+          </div>
         </div>
+          <div className="personality-container">
+            <div className="personality-menu-container">
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column', 
+                alignItems: 'center', 
+                '& a': {
+                  color: 'white', 
+                  marginTop: 2, 
+                  textDecoration: 'none', 
+                  width: '100%', 
+                  textAlign: 'center',
+                  paddingLeft: 20,
+                  paddingRight: 20,
+                  font: "Verdana" 
+                }
+              }}
+            >
+              <Autocomplete
+                id="trait-autocomplete"
+                options={hardcodedPersonalityTraits}
+                getOptionLabel={(option) => option}
+                value={selectedTrait}
+                onChange={handleTraitChange}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Select a Trait"
+                    variant="outlined"
+                    fullWidth
+                    sx={{
+                      "& .MuiOutlinedInput-root": {
+                        bgcolor: '#E0E0E0',
+                        color: 'black',
+                        "&.Mui-focused": {
+                          bgcolor: "#E0E0E0",
+                        },
+                        "& fieldset": {
+                          borderColor: "#E0E0E0",
+                        },
+                        "&:hover fieldset": {
+                          borderColor: "#E0E0E0",
+                        },
+                        "&.Mui-focused fieldset": {
+                          borderColor: "#E0E0E0",
+                        },
+                      },
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: '#333333',
+                      },
+                      "&:hover .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#E0E0E0",
+                      },
+                      "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                        borderColor: 'black',
+                      }
+                    }}
+                  />
+                )}
+                sx={{
+                  width: 300,
+                  marginBottom: 2, 
+                  "& .MuiAutocomplete-inputRoot": {
+                    color: "#E0E0E0",
+                  },
+                }}
+              />
+              <a href="#" style={{ color: 'white'}}>Select a personality to display the correlation between it and the users' rating per genre</a>
+            </Box>
+            </div>
+            <div className="personality-histogram-container ">
+              {selectedTrait && <GenrePersonalitysHistogram personalityGenres={personalityGenres} selectedTrait={selectedTrait || hardcodedPersonalityTraits[0]} />}
+            </div>
+          </div>
+          </div>
+      </>
       )}
     </div>
     </ThemeProvider>
